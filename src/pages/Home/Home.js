e7698d9136fef92dea7ca1b32c5c846bfa18c4dc
alishahi <shahy8227@gmail.com>
2025-02-16 03:47:23 +0330

    carts

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../../components/banner/Banner'
+import HouseList from '../../components/carts/HouseList'
 
 function Home() {
   return (
     <div>
       <Banner/>
+      <HouseList/>
     </div>
   )
 }
