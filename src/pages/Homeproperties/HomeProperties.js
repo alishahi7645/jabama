8a408ae143eef06d87a8a57612c351d4812dd7a7
alishahi <shahy8227@gmail.com>
2025-02-16 04:21:06 +0330

    carts property

--- src/pages/Homeproperties/HomeProperties.js
+++ src/pages/Homeproperties/HomeProperties.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { housesData } from '../../data';
 import { useParams } from 'react-router-dom';
 import { BiBed, BiBath, BiArea, BiPhone } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
+
 
 const PropertyDetails = () => {
   const { id } = useParams();
